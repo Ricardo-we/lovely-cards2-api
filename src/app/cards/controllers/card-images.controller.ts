6078d22f21b9008a8053c191711993c222531f7e
Ricardo-we <totgamer75@gmail.com>
2023-01-01 00:30:26 -0600

    Happy new year 2023!

---
+++ src/app/cards/controllers/card-images.controller.ts
@@ -0,0 +1,78 @@
+import BController, { ControllerConfig } from "../../../utils/Base/BController";
+import { Card, CardImage, CardMessage } from "../model";
+import { Request, Response } from "express";
+
+import CardService from "../services/CardService";
+import { CloudinaryUploader } from "../../../services/file-upload/Uploaders/CloudinaryUploader";
+import { ICard } from "../../../types/Cards";
+import { compareTwoDates } from "../../../utils/date.utils";
+import { errorResponse } from "../../../utils/controller.utils";
+
+export default class CardsController implements BController {
+    private fileUploader: CloudinaryUploader;
+    constructor(
+        public controllerConfig: ControllerConfig,
+    ) {
+        this.fileUploader = new CloudinaryUploader();
+    }
+
+    post = async (req: Request | any, res: Response) => {
+        try {
+            const card = req.body as ICard
+            const music = req?.files.length > 0 ? req?.files[0] : card?.music || null;
+            const newCard = await CardService.createCard(card, req.user, music);
+            return res.json(newCard);
+        } catch (error) {
+            return res.json(errorResponse(error))
+        }
+    }
+
+    put = async (req: Request | any, res: Response) => {
+        try {
+            const user = req?.user;
+            const card = req.body as ICard
+            const music = req?.files[0];
+            const updatedCard = await CardService.updateCard(card, req.params.cardId, user, music);
+            return res.json(updatedCard);
+        } catch (error) {
+            return res.json(errorResponse(error))
+        }
+    }
+
+    async destroy(req: Request, res: Response) {
+
+    }
+
+    async get(req: any, res: Response) {
+        try {
+            const userCards = await Card.findAll({
+                where: { user_id: req.user.id },
+            });
+            return res.json(userCards);
+        } catch (error) {
+            return res.json(errorResponse(error))
+        }
+    }
+
+    async getOne(req: Request, res: Response) {
+        try {
+            const { cardId } = req.params;
+            const cardMessages = await CardMessage.findAll({ where: { card_id: cardId } });
+            const cardImages = await CardImage.findAll({ where: { card_id: cardId } });
+            const card = await Card.findOne({ where: { id: cardId }, });
+
+
+            const result = {
+                ...card?.toJSON(),
+                cardContents: [...cardMessages, ...cardImages].sort(
+                    (current, prev) =>
+                        compareTwoDates(current.getDataValue("createdAt"), prev.getDataValue("createdAt")),
+                ).map(item => item.toJSON()),
+            };
+
+            return res.json(result);
+        } catch (error) {
+            return res.json(errorResponse(error))
+        }
+    }
+}
